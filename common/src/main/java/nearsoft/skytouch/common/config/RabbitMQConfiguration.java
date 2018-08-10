df72b90e5b0f71e08dc8e12215216d9f3610c98f
Jose Juan Juarez Monsivais <jjuarez@nearsoft.com>
2018-08-09 21:24:31 -0500

    changed the RabbitConfiguration to constants

--- common/src/main/java/nearsoft/skytouch/common/config/RabbitMQConfiguration.java
+++ common/src/main/java/nearsoft/skytouch/common/config/RabbitMQConfiguration.java
@@ -1,14 +1,12 @@
 package nearsoft.skytouch.common.config;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class RabbitMQConfiguration {
 
-    @Value("${channel-config.create-products-channel-name}")
     public static final String CREATE_PRODUCTS_CHANNEL_NAME = "createProductsChannel";
-    @Value("${channel-config.request-products-channel-name}")
     public static final String REQUEST_PRODUCTS_CHANNEL_NAME = "requestProductsChannel";
-    @Value("${channel-config.receive-products-channel-name}")
     public static final String RECEIVE_PRODUCTS_CHANNEL_NAME = "receiveProductsChannel";
 
 }
